582d00cac17f8cb66c62b25f7027386e8f2092ce
karun <1harunkorkmaz@gmail.com>
2025-04-16 16:30:16 +0300

    Enhance Main component with expense management features
    - Introduced AddExpense component for adding new expenses directly in the Main view.
    - Replaced ExpenseList with a structured Card and Table layout to display expenses.
    - Integrated useExpenseContext to manage and display expenses dynamically.
    - Improved UI layout for better user experience and organization.

--- app/pages/Main.tsx
+++ app/pages/Main.tsx
@@ -1,18 +1,43 @@
-import { Button } from 'react-bootstrap';
-import Link from 'next/link';
-import ExpenseList from '../components/ExpenseList';
+'use client';
+
+import ExpenseItem from '../components/ExpenseItem';
+import { Card, Table } from 'react-bootstrap';
+import { useExpenseContext } from '../contexts/ExpenseContext';
+import AddExpense from '../components/AddExpense';
 
 export default function Main() {
+    const { expenses } = useExpenseContext();
     return (
         <div className="container mt-4">
             <div className="d-flex justify-content-between align-items-center mb-4">
-                <h2>Expenses</h2>
-                <Link href="/add">
-                    <Button variant="primary">Add New Expense</Button>
-                </Link>
+                <div className="d-flex flex-grow-1 flex-column">
+                    <h2>Expenses</h2>
+                    <div>
+                        <AddExpense />
+                    </div>
+                </div>
             </div>
             <div>
-                <ExpenseList />
+                <Card>
+                    <Card.Body>
+                        <Table hover responsive>
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Amount</th>
+                                    <th>Category</th>
+                                    <th>Date</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {expenses.map((expense) => (
+                                    <ExpenseItem key={expense.id} expense={expense} />
+                                ))}
+                            </tbody>
+                        </Table>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     );
